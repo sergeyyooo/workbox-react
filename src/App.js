539a6366b27fcf679d48ccf4a4ac76f79bfeb7c9
Your Name <you@example.com>
2021-08-08 18:17:48 +0300

    added service worker file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { div } from 'prelude-ls';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() =>{
+    const createData = () =>{
+      const arr = []
+      for (let i = 0; i<30; i++) arr.push(i)
+      setData(arr)
+    }
+    createData()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {
+        data.map(i=><div className = 'cube'></div>)
+      }
+     </div>
   );
 }
 
