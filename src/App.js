1373eb0c481d29d85e4bea13bd8312eb5b626a44
Your Name <you@example.com>
2021-08-10 20:47:34 +0300

    added upload from swapi.dev

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import { div } from 'prelude-ls';
 function App() {
 
   const [data, setData] = useState([])
+  const [name, setName] = useState('')
 
   useEffect(() =>{
+    const fetchData = async()=>{
+      const res = await fetch('https://swapi.dev/api/people/11/')
+      const data = await res.json()
+      setName(data?.name)
+    }
     const createData = () =>{
       const arr = []
       for (let i = 0; i<30; i++) arr.push(i)
       setData(arr)
     }
     createData()
+    fetchData()
   },[])
 
   return (
     <div className="App">
+      <h3>{name}</h3>
       {
-        data.map((i, idx)=><div className = 'cube' key = {i}>{i}</div>)
+        data?.map((i, idx)=><div className = 'cube' key = {i}>{i}</div>)
       }
      </div>
   );
