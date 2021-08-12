6da37e82e2f251165deb85b788e31b095d930cfc
Your Name <you@example.com>
2021-08-12 21:14:51 +0300

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,88 @@
-import React, {useState,useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { div } from 'prelude-ls';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
-function App() {
-
-  const [data, setData] = useState([])
-  const [name, setName] = useState('')
-
-  useEffect(() =>{
-    const fetchData = async()=>{
-      const res = await fetch('https://swapi.dev/api/people/11/')
-      const data = await res.json()
-      setName(data?.name)
-    }
-    const createData = () =>{
-      const arr = []
-      for (let i = 0; i<30; i++) arr.push(i)
-      setData(arr)
-    }
-    createData()
-    fetchData()
-  },[])
-
+export default function App() {
   return (
-    <div className="App">
-      <h3>{name}</h3>
-      {
-        data?.map((i, idx)=><div className = 'cube' key = {i}>{i}</div>)
-      }
-     </div>
+    <Router>
+      <div>
+        <ul style = {{display:'flex', justifyContent:'space-between', margin:'10px', backgroundColor:'yellow'}}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2 style = {{height:'100vh', backgroundColor:'green'}}>Home</h2>;
+}
+
+function About() {
+  return <h2 style = {{height:'100vh', backgroundColor:'blue'}}>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div style = {{height:'100vh', backgroundColor:'orange'}}>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3 >Requested topic ID: {topicId}</h3>;
+}
